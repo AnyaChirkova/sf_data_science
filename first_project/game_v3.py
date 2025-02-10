eb99ae66e2369f7ec5c7118cccaba8734a74239f
AnyaChirkova <chirkova.ab@gmail.com>
2025-02-10 11:33:06 +0300

    commit

--- first_project/game_v3.py
+++ first_project/game_v3.py
@@ -22,7 +22,7 @@ import numpy as np
 """
 
 def score_game(random_predict) -> int:
-    """За какое количество попыток в среднем за 10000 подходов угадывает наш алгоритм
+    """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
 
     Args:
         random_predict ([type]): функция угадывания
@@ -32,7 +32,7 @@ def score_game(random_predict) -> int:
     """
     count_ls = []
     #np.random.seed(1)  # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(10000))  # загадали список чисел
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
 
     for number in random_array:
         count_ls.append(random_predict(number))
